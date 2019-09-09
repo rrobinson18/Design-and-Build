e098e41dd6b3061100c429d17ddfdb3f1f29b072
rrobinson18 <robinson.rakesha@gmail.com>
2019-09-09 00:24:56 -0400

    animate img

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ function init() {
   const slides = document.querySelectorAll(".slide");
   const pages = document.querySelectorAll(".page");
   const backgrounds = [
-    `radio.gradient(#2B3760, #0B1023)`,
-    `radio.gradient(#4E3022, #161616)`,
-    `radio.gradient(#4E4342, #0B1023)`
+    `radial-gradient(#2B3760, #0B1023)`,
+    `radial-gradient(#4E3022, #161616)`,
+    `radial-gradient(#4E4342, #0B1023)`
   ];
 
   //Tracker
@@ -36,13 +36,21 @@ function init() {
 
     const tl = new TimelineMax();
 
-    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" }).fromTo(
-      currentRight,
-      0.3,
-      { y: "10%" },
-      { y: "-100%" },
-      "-=0.2"
-    );
+    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
+    .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
+    .to(portfolio, 0.3, {backgroundImage: backgrounds[pageNumber] })
+    .fromTo(currentPage, 0.3, {opacity:1, pointerEvents: 'all'}, {opacity:0, pointerEvents: 'none'})
+    .fromTo(nextPage, 0.3, {opacity:0, pointerEvents: 'none'}, {opacity:1, pointerEvents: 'all'}, '-=0.6')
+    .fromTo(nextLeft, 0.3, {y: '-100'}, {y: '-10'}, '-=0.6')
+    .fromTo(nextRight, 0.3, {y: '-100'}, {y: '10'}, '-=0.8')
+    .fromTo(nextText, 0.3, {opacity: 0, y: 0}, {opacity: 1, y: 0})
+    .set(nextLeft, {clearProps: 'all'})
+    .set(nextRight, {clearProps: 'all'})
+
+
+    current = pageNumber;
+
+
   }
 }
 
